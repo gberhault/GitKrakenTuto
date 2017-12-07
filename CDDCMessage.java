3f4d8604187c182debeb76a19b7627d4dc889243
Unknown <guillaume.berhault33@gmail.com>
2017-12-07 08:53:21 -0800

    New: #00001 - New file for test

---
+++ CDDCMessage.java
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) since 1999. VASCO DATA SECURITY
+// All rights reserved. http://www.vasco.com
+//
+////////////////////////////////////////////////////////////////////////////////
+
+package com.vasco.orchestration.client.cddc;
+
+public class CDDCMessage {
+
+    /**
+     * the CDDC Return Code
+     *
+     */
+    private int returnCode;
+
+    /**
+     * JSON string containing the CDDC message
+     *
+     */
+    private String cddcMessage;
+
+    public CDDCMessage(String message, int returnCode) {
+        this.cddcMessage = message;
+        this.returnCode = returnCode;
+    }
+
+    public int getReturnCode() {
+        return returnCode;
+    }
+
+    public String getCddcMessage() {
+        return cddcMessage;
+    }
+
+
+}
